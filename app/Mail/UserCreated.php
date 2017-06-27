2a7c162cb08f436a2a48d5a14fbafcba2ea0c25d
˜Robert <robstermarinho@gmail.com>
2017-06-27 16:32:03 -0300

    Creating the Mail Class

---
+++ app/Mail/UserCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        // Laravel automatcally injects this public atribute on the build method
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        // Text Plain
+        return $this->text('emails.welcome');
+    }
+}
---
+++ files/commands/mail.php
@@ -0,0 +1,4 @@
+<?php
+
+// Create a mail class
+php artisan make:mail UserCreated
