9ab308e3994164614bb1cf6c5ceb715ce62086ea
˜Robert <robstermarinho@gmail.com>
2017-06-28 12:04:16 -0300

    Mail Changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\User;
 use App\Product;
+use App\Mail\UserCreated;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,6 +29,26 @@ class AppServiceProvider extends ServiceProvider
             }
         });
 
+        // Listen the update event of an user
+        User::updated(function($user) {
+            // If the email has been edited since it was queried from the database
+            if ($user->isDirty('email')) {
+                retry(5, function() use ($user) {
+                    Mail::to($user)->send(new UserMailChanged($user));
+                }, 100);
+            }
+        });
+
+        // After a new user is created
+        User::created(function($user){
+
+            if ($user->isDirty('email')) {
+                retry(5, function() use ($user) {
+                    Mail::to($user)->send(new UserCreated($user));
+                }, 100);
+            }
+            
+        });
         
     }
 
